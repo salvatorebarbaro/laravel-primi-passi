a20a8b21f39e71efad07331db20f5480e113e534
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-04-09 02:11:32 +0200

    esecuzione del bonus

--- resources/views/ciao.blade.php
+++ resources/views/ciao.blade.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Prima pagina</title>
     
 </head>
 <body>
+    <nav>
+        <ul>
+            <li>
+                <a href="{{route('seconda')}}">Seconda pagina</a>
+            </li>
+        </ul>
+    </nav>
 
-
+<!-- richiamo i dati inseriti con la funzione compact -->
 <h1 style="text-align: center">{{$titolo}}</h1>
 
 <p style="text-align:center;">{{$testo}}</p>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// Quindi sì, puoi considerare questo codice come una definizione di una rotta che indirizza Laravel a gestire le richieste alla homepage del tuo sito web e a restituire i dati alla vista ciao.
 Route::get('/', function () {
 
     $titolo="Prima pagina con laravel";
@@ -23,3 +23,12 @@ Route::get('/', function () {
     // Create array containing variables and their values . Definizione di compact
     return view('ciao',compact('titolo','testo') );
 });
+
+Route::get('/seconda', function () {
+
+    $titoloSeconda="Seconda pagina di laravel";
+
+
+    // Create array containing variables and their values . Definizione di compact
+    return view('second',compact('titoloSeconda') );
+})->name('seconda');
